6acc6eecefb02337cb674e898b3fa8fa2d3e2660
TheSnowly <thesnowly18@gmail.com>
2026-01-26 11:08:19 +0100

    [Micka] - MC raycast Ft type setup / Level 1 integration

---
+++ Assets/Audio_Slope/ScriptsAudio/AudioSurfaceTypeEnum.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SurfaceType
+{
+    Dirt,
+    Grass,
+    Mud,
+    Stone
+}
+
+public class AudioSurfaceTypeEnum : MonoBehaviour
+{
+    public SurfaceType surfaceType;
+}
--- Assets/Audio_Slope/ScriptsAudio/PlayerAudioManager.cs
+++ Assets/Audio_Slope/ScriptsAudio/PlayerAudioManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAudioManager : MonoBehaviour
@@ -16,24 +17,40 @@ public class PlayerAudioManager : MonoBehaviour
     public AK.Wwise.Event deathEvent;
     public AK.Wwise.Event presenceEvent;
 
-    // Start is called before the first frame update
+    //set the surface type the player is walking on 
+    void SetFTSurfaceType()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit))
+        {
+            AudioSurfaceTypeEnum surfaceType = hit.collider.GetComponent<AudioSurfaceTypeEnum>();
+            if (surfaceType == null) return;
+
+            AkSoundEngine.SetSwitch("FT_Type", surfaceType.surfaceType.ToString(), this.gameObject);
+        }
+    }
+
     public void AudioWalk()
     {
+        SetFTSurfaceType();
         walkEvent.Post(gameObject);
     }
 
     public void AudioLand()
     {
+        SetFTSurfaceType();
         landEvent.Post(gameObject);
     }
 
     public void AudioJump()
     {
+        SetFTSurfaceType();
         jumpEvent.Post(gameObject);
     }
 
     public void AudioRoll()
     {
+        SetFTSurfaceType();
         rollEvent.Post(gameObject);
     }
 
@@ -49,6 +66,7 @@ public class PlayerAudioManager : MonoBehaviour
 
     public void AudioScuff()
     {
+        SetFTSurfaceType();
         scuffEvent.Post(gameObject);
     }
 
